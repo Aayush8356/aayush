847299459073747e5d90d5a3d88fba7411ab107e
Aayush Gupta <aayushgupta@Aayushs-MacBook-Air.local>
2024-12-16 20:33:48 +0530

    gradiant change

--- src/info/Info.js
+++ src/info/Info.js
@@ -10,7 +10,7 @@ import mock5 from "../img/stonePaperScissor.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(65, 234, 83)", "rgb(235, 185, 37)"];
+export let colors = ["rgb(223, 145, 12)", "rgb(135, 135, 135)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
